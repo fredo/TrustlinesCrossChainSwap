bf5a401efdaa0b7e273d9cd1c2b370dbca61c878
Marius van der Wijden <m.vanderwijden@live.de>
2020-01-14 23:31:52 +0100

    [contracts] Updated EtherSwap to solc 0.6, multiple fixes

--- contracts/EtherSwap.sol
+++ contracts/EtherSwap.sol
@@ -1,68 +1,60 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.6.0;
 
 
 contract EtherSwap {
 
     struct Swap {
-        address inititator;
-        address recipient;
-        uint256 endTimeStamp;
+        address payable inititator;
+        uint64 endTimeStamp;
+        address payable recipient;
         uint256 value;
     }
 
     mapping(bytes32 => Swap) public swapMap; // the key is the hash
 
-    event SwapInitiatedEvent(bytes32 indexed hash, uint256 indexed value);
-    event SwapSuccessEvent(bytes32 indexed hash, uint256 indexed value);
+    event SwapInitiatedEvent(bytes32 indexed hash, uint256 value);
+    event SwapSuccessEvent(bytes32 indexed hash, uint256 value);
+    event SwapExpiredEvent(bytes32 indexed hash);
 
-    function secretLock(uint256 _lockTimeSec, bytes32 _hash, address _recipient) external payable returns (bool) {
+    function secretLock(uint64 _lockTimeSec, bytes32 _hash, address payable _recipient) external payable {
         require(swapMap[_hash].inititator == address(0x0), "Entry already exists");
         require(msg.value > 0, "Ether is required");
 
         swapMap[_hash].inititator = msg.sender;
         swapMap[_hash].recipient = _recipient;
-        swapMap[_hash].endTimeStamp = now + _lockTimeSec;
+        swapMap[_hash].endTimeStamp = uint64(now + _lockTimeSec);
         swapMap[_hash].value = msg.value;
 
         emit SwapInitiatedEvent(_hash, msg.value);
-        return true;
     }
 
-    function secretProof(bytes32 _hash, bytes _proof) external returns (bool) {
-        require(swapMap[_hash].inititator != address(0x0), "No entry found"); 
-        require(swapMap[_hash].endTimeStamp >= now, "TimeStamp violation");
+    function secretProof(bytes calldata _proof) external {
+        bytes32 hash = sha256(_proof);
+        require(swapMap[hash].inititator != address(0x0), "No entry found"); 
+        require(swapMap[hash].endTimeStamp >= now, "TimeStamp violation");
 
-        bytes32 calculatedHash = sha256(_proof);
-        require(calculatedHash == _hash, "Hash check failed");
-
-        uint256 value = swapMap[_hash].value;
-        address recipient = swapMap[_hash].recipient;
+        uint256 value = swapMap[hash].value;
+        address payable recipient = swapMap[hash].recipient;
     
-        clean(_hash);
+        clean(hash);
         recipient.transfer(value);
-        emit SwapSuccessEvent(_hash, value);
+        emit SwapSuccessEvent(hash, value);
     }
 
-    function swapExpiredRefund(bytes32 _hash) external returns (bool) {
+    function swapExpiredRefund(bytes32 _hash) external {
         require(swapMap[_hash].inititator != address(0x0), "No entry found");
         require(swapMap[_hash].endTimeStamp < now, "TimeStamp violation");
 
         uint256 value = swapMap[_hash].value;
-        address initiator = swapMap[_hash].inititator;
+        address payable initiator = swapMap[_hash].inititator;
         clean(_hash);
         
         initiator.transfer(value);
-        return true;
-    }
-
-    function calcHash(bytes _proof) external pure returns (bytes32) {
-        return sha256(_proof);
+        emit SwapExpiredEvent(_hash);
     }
 
     function clean(bytes32 _hash) private {
-
         Swap storage swap = swapMap[_hash];
-
         delete swap.inititator;
         delete swap.recipient;
         delete swap.endTimeStamp;
